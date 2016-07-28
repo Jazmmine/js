458202a1f8ad6949d36b0b1131459c29a011ee97
Jazmmine <jazmine.angoma@gmail.com>
2016-07-27 19:28:49 -0500

    jueguito

---
+++ app.js
@@ -0,0 +1,35 @@
+var aleatorio = Math.random()*10;
+var numero = Math.floor(aleatorio);
+
+/*alert("Cómo lo supo?");*/
+/*promt("Cómo lo supo?");*/
+/*var mensaje = prompt("Cómo lo supo?");
+alert("Hola " + mensaje);*/
+
+/*var numero = Math.random()*10;
+alert("Numero " + numero);*/
+
+/*var aleatorio = Math.random()*10;
+var numero = Math.floor(aleatorio);
+alert("Numero " + numero);*/
+
+/*
+var numero = 10;
+
+if (numero != 10) {
+	console.log("Còmo lo supo");
+	console.log("xD");
+}
+else{
+	console.log(":O");
+}
+*/
+var numeroUsuario = prompt("Ingresa un número (del 1 al 10)");
+
+if (numero == numeroUsuario) {
+	alert("Cómo lo supo?");
+	console.log("xD");
+}
+else{
+	alert("-.- El numero real fue" + numero);
+}
